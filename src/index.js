02f25a42e21eb2f03dbe1cdbb134479c16c4218a
rajnandini <rajnandiniab@gmail.com>
2021-04-16 14:19:28 +0530

    testing app purposes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const books = [
+{
+  id:1,
+ img: 'https://images-eu.ssl-images-amazon.com/images/I/417-p2FWoJL._AC_SX368_.jpg',
+ title:'I love you to the moon and back',
+ author: 'Amelia Hepworth'
+},
+{
+  id:2,
+  img: 'https://images-na.ssl-images-amazon.com/images/I/51wlUnNtsHL._AC_SX368_.jpg',
+  title:'Our class is a Family',
+  author: 'Shannon Olsen'
+ },
+ {
+  id:3,
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81-ivK3sKGL.__BG0,0,0,0_FMpng_AC_UL210_SR210,210_.jpg',
+  title:'Win',
+  author: 'Brit Bennet'
+ },
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList() {
+  return (
+     <section className="booklist">
+     {books.map((book) => {
+       const {img, title, author } = book;
+      return  <Book key={book.id} {...book}></Book>;
+        
+     })}</section>
+  );
+}
+
+const Book = ({img, title, author }) => {
+ 
+//attribute,eventHandler
+//onClick, onMouseOver
+const clickHandler = () => {
+     alert('hello world');
+};
+  return <article className="book">
+    <img src={img}></img>
+    <h1  onClick={clickHandler}>{title}</h1>
+    <h4>{author}</h4>
+    <button type="button" onClick={clickHandler}></button>
+  </article>;
+
+};
+
+ReactDom.render(<BookList/>, document.getElementById('root'));
